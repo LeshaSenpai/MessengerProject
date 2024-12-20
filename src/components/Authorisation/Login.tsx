a5547a314e892457a27c212a5b459a7e593347ab
LeshaSenpai <134790759+LeshaSenpai@users.noreply.github.com>
2024-12-20 10:39:48 +0300

    validation has been added to the project

--- src/components/Authorisation/Login.tsx
+++ src/components/Authorisation/Login.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import "./Login.css";
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (!username || !password) {
+      setError("Please fill out all fields.");
+      return;
+    }
 
     try {
       const response = await fetch("http://localhost:3001/users");
@@ -30,6 +38,29 @@ const Login: React.FC = () => {
     }
   };
 
+  const handleUsernameBlur = () => {
+    if (!username.trim()) {
+      setUsernameError("Требуется ввести юзернейм.");
+    } else if (username.length < 5 || username.length > 15) {
+      setUsernameError("Имя пользователя должно содержать от 5 до 15 символов.");
+    } else {
+      setUsernameError("");
+    }
+  };
+
+  const handlePasswordBlur = () => {
+    const hasLetters = /[a-zA-Z]/.test(password);
+    const hasNumbers = /[0-9]/.test(password);
+
+    if (!password.trim()) {
+      setPasswordError("Требуется ввести пароль.");
+    } else if (!hasLetters || !hasNumbers) {
+      setPasswordError("Пароль должен содержать как буквы, так и цифры.");
+    } else {
+      setPasswordError("");
+    }
+  };
+
   return (
     <div>
       <h2>Login</h2>
@@ -41,7 +72,9 @@ const Login: React.FC = () => {
             id="username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            onBlur={handleUsernameBlur}
           />
+          {usernameError && <p className="error-message">{usernameError}</p>}
         </div>
         <div>
           <label htmlFor="password">Password:</label>
@@ -50,10 +83,12 @@ const Login: React.FC = () => {
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            onBlur={handlePasswordBlur}
           />
+          {passwordError && <p className="error-message">{passwordError}</p>}
         </div>
         <button type="submit">Login</button>
-        {error && <p style={{ color: "red" }}>{error}</p>}
+        {error && <p className="error-message">{error}</p>}
       </form>
     </div>
   );
